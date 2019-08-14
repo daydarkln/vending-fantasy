e5ea3d921abbf782bb174a90240e74cb889feecd
Egor <daydarkez@gmail.com>
2019-08-14 23:43:33 +0300

    rest init

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import "./index.css";
 import App from "./App";
 import store from "./store";
 import { register } from "./serviceWorker";
+import { RestfulProvider } from "restful-react";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <RestfulProvider base="http://localhost:3001">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </RestfulProvider>,
   document.getElementById("root")
 );
 register();
